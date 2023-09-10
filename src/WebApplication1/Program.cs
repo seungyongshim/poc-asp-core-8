4fb0967c7e65a40204f7645142ae596a9481a7b5
SeungYongShim <syshim@me.com>
2023-09-10 11:35:35 +0900

    .

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,10 +1,16 @@
 using System.Diagnostics;
+using WebApplication1.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.PropertyNameCaseInsensitive = true;
+    options.SerializerOptions.WriteIndented = true;
+    options.SerializerOptions.IncludeFields = true;
+});
 
 var app = builder.Build();
 
@@ -16,17 +22,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpLogging();
-
 app.MapHealthChecks("/health");
 
-app.MapGet("/", () =>
+app.MapGet("/", (HttpContext ctx) => TypedResults.Ok(new
 {
-    return new
-    {
-        TraceId = Activity.Current?.Id ?? ""
-    };
-})
-.WithName("GetWeatherForecast")
+    Text = "Hello"
+}))
+.AddEndpointFilter<ResponseAddTraceIdFilter>()
 .WithOpenApi();
 
 app.Run();
